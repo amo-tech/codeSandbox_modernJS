0e1744a71d5c883356987f7c9f7d775f7b6868a5
amo-tech <kaomi.x1003@gmail.com>
2022-10-11 09:49:00 +0900

    分割代入、デフォルト値

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const name = "kaomi";
+//const name = "kaomi";
 
-const age = 28;
+//const age = 28;
 //　テンプレート文字列
 const message = `my name is ${name}. I'm ${age} years old.`;
 console.log(message);
@@ -27,3 +27,21 @@ console.log(funcArrStr('ccc'));
 
 const funcAddNum = (num1, num2) => num1 + num2;
 console.log(funcAddNum(1, 3));
+
+// 分割代入
+const profile = {
+  name: "kaomi",
+  age: 28,
+};
+const { name, age } = profile;
+const message101101 = `my name is ${name}. I'm ${age} years old`;
+console.log(message101101);
+
+const profileShime = ["shime", 30];
+const [ nameShime, ageShime ] = profileShime;
+const message101102 = `my name is ${nameShime}. I'm ${ageShime} years old`;
+console.log(message101102);
+
+// デフォルト値、引数
+const sayHello = (name = "guest") => console.log(`Welcome, ${name}!`);
+sayHello(); // 引数を入れないとundefinedになる
