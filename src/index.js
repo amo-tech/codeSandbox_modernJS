27cfa2db43cb83862e797ac5af205afec183e9c9
amo-tech <kaomi.x1003@gmail.com>
2022-10-17 08:29:14 +0900

    イベントリスナー追加ボタン

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("a");
+import "./styles.css";
+
+const onClickAdd = () => {
+  alert();
+  console.log("追加ボタン押しました")
+};
+
+document.getElementById("add-button").addEventListener("click", () => onClickAdd());　//イベントリスナー調べる
