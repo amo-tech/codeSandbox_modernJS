9e25a661f6a8f6ba5b4a7b3b61fd9ced73c418be
amo-tech <kaomi.x1003@gmail.com>
2022-10-19 19:01:39 +0900

    createElement修正

--- src/index.js
+++ src/index.js
@@ -2,9 +2,29 @@ import "./styles.css";
 
 const onClickAdd = () => {
   // テキストボックスの値を取得し、初期化する
-  const inputText = document.getElementById("add-text").value
-  document.getElementById("add-text").value = ""
-  alert(inputText);
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // li作成
+  const li = document.createElement("li");
+  console.log(li);
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  console.log(div);
+
+  // span生成
+  const span = document.createElement("span");
+  span.innerText = inputText;
+  console.log(span);
+
+  // li生成
+  // const li = document.createElement("li");
+  // li.innerText = inputText;
+
+  // 子要素に各要素を追加
+
 };
 
 document.getElementById("add-button").addEventListener("click", () => onClickAdd());　//イベントリスナー調べる
